36ad90770fac9433e36dd44fdb09515607952eb5
syaoranea <83190304+syaoranea@users.noreply.github.com>
2021-08-29 10:47:33 -0300

    update index

--- assets/php/cadastrardados.php
+++ assets/php/cadastrardados.php
@@ -19,14 +19,10 @@ if(isset($_POST['bt-cadastrar'])){
     $fixo = addslashes($_POST['fixo']);
     $celular = addslashes($_POST['celular']);
 
-    try{
+    
     if($c->cadastrar($nome, $cpf, $rg, $sexo, $endereco, $numero, $bairro, $cidade, $estado, $cep, $fixo, $celular) == true){
 
-        header("Location: sucesso.html");
+        header("Location: ../../sucesso.html");
     }
-    }catch(PDOException $e){
-    echo "Erro: ".$e->getMessage();
-    }
-
-
+    
 }
